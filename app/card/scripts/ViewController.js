631507e111692e4895e71c24a266bf9a1a47d721
fparedesg <fparedesgarza@gmail.com>
2015-11-03 11:38:42 -0600

    Prettified contact views. Information is now formatted better: (e.g. phone numbers have a phone format if they have a length of 10, and dates have time zone information stripped out). If user taps on phone or email, it calls and opens up a blank email, respectively.

--- app/card/scripts/ViewController.js
+++ app/card/scripts/ViewController.js
@@ -31,7 +31,23 @@ angular
 		$scope.OpenEdit = function(card_id) {
 			// Open Edit
 			supersonic.ui.modal.show("card#edit?id="+card_id);
-
 		};
+
+		$scope.FormatPhone = function(phoneNumber) {
+			var str = phoneNumber.toString();
+			if(str.length == 10) {
+				return "(" + str.substring(0, 3) + ") " + str.substring(3, 6) + "-" + str.substring(6, 10);
+			}
+			return str;
+		}
+
+		// Tue Nov 03 2015 11:18:07 GMT-0600 (CST)
+		var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		$scope.FormatDate = function(lastContact) {
+			return weekDays[lastContact.getDay()] + ", " +
+				months[lastContact.getMonth()] + " " + lastContact.getDate() + ", " + lastContact.getFullYear() +
+				" at " + lastContact.getHours()%12 + ":" + lastContact.getMinutes() + (lastContact.getHours()>12?" PM":" AM")
+		}
 		
 	});
